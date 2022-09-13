ba403bea4def3ef946dd6018f3cf4e914fc4d066
Pradeep <ps178@snu.edu.in>
2022-09-13 18:58:37 +0530

    added debug feature

--- scripts/source/run.sh
+++ scripts/source/run.sh
@@ -1,3 +1,3 @@
 read file
 g++ -Wall -Wextra -pedantic -std=c++2a -O2 -Wshadow -Wformat=2  -Wshift-overflow=2  -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector  "$file.cpp" -o a;
-a < in
+./a < in
